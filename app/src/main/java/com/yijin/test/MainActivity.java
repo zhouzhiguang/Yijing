310f9658227cf3d9f8ac91c422ce0c8174e18ec5
Administrator <596889153@qq.com>
2018-02-28 14:32:47 +0800

    第一次提交

--- app/src/main/java/com/yijin/test/MainActivity.java
+++ app/src/main/java/com/yijin/test/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         maintext = (TextView) findViewById(R.id.main_text);
 
+   .
 
     }
 }
